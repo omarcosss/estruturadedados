4fc313412862f1fdeb3b7b600ccd599e0e3377f5
Marcos <omarcosss@outlook.com>
2024-03-11 13:59:14 -0300

    até 4.10 'alocação dinâmica'

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,25 +12,72 @@ public class Main {
         Aluno a1 = new Aluno();
         Aluno a2 = new Aluno();
         Aluno a3 = new Aluno();
+        Aluno a4 = new Aluno();
+        Aluno a5 = new Aluno();
 
         a1.setNome("Glevson");
         a2.setNome("Francisco");
         a3.setNome("Maria");
+        a4.setNome("fjhsid");
+        a5.setNome("ndsaku");
 
         System.out.println("Alunos: "+ a2.getNome());
         System.out.println("Alunos: "+ a1.getNome());
         System.out.println("Alunos: "+ a3.getNome());
 
-        lista.Adiciona(a1);
-        lista.Adiciona(a2);
-        lista.Adiciona(a3);
+        //adicionando alunos à lista
+        lista.adiciona(0, a1);
+        lista.adiciona(1, a2);
+        lista.adiciona(2,a3);
 
+        //imprimindo tamanho da lista
         System.out.println("Total de alunos: " + lista.tamanho());
 
+        //imprimindo lista inicial
         System.out.println(lista);
 
-        System.out.println("Contém a1: " + lista.contem(a1));
+        //verificando se lista contem aluno por nome
+        System.out.println("Contém aluno: " + lista.contem("glevson"));
 
-        System.out.println(lista.pega(3));
+        //Buscando aluno por posição na lista
+        System.out.println(lista.pega(2));
+
+        //Adicionando novo aluno na posição 3
+        lista.adiciona(3, a4);
+
+        //Conferindo
+        System.out.println(lista);
+
+        //Adicionando novo aluno em posição já ocupada
+        lista.adiciona(1, a5);
+
+        //Conferindo novo aluno em posição de substituição
+        System.out.println(lista);
+
+        //Removendo aluno da lista
+        lista.remove(0);
+
+        //Conferindo
+        System.out.println(lista);
+
+
+
+
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Buscar aluno: ");
+//        String busca = scanner.nextLine();
+//
+//
+//
+//        if (lista.contem(busca)) {
+//            System.out.println("ALUNO ENCONTRADO: " + busca.toUpperCase() + "POSIÇÃO: " + );
+//        } else {
+//            System.out.println("Aluno não encontrado!");
+//        }
+
     }
 }
--- src/Vetor.java
+++ src/Vetor.java
@@ -3,8 +3,22 @@ public class Vetor {
 
     private int totalDeAlunos = 0;
 
-    public void Adiciona(Aluno aluno){
-        this.alunos[this.totalDeAlunos]= aluno;
+    public void adiciona(Aluno aluno) {
+        this.garantaEspaco();
+        this.alunos[this.totalDeAlunos] = aluno;
+        this.totalDeAlunos++;
+    }
+
+    public void adiciona(int posicao, Aluno aluno){
+        this.garantaEspaco();
+        if (!this.posicaoValida(posicao)) {
+            throw new IllegalArgumentException("Posição inválida!");
+        }
+
+        for (int i = this.totalDeAlunos - 1; i >= posicao ; i -= 1) {
+            this.alunos[i + 1] = this.alunos[i];
+        }
+        this.alunos[posicao] = aluno;
         this.totalDeAlunos++;
     }
 
@@ -25,16 +39,28 @@ public class Vetor {
     }
 
     private boolean posicaoOcupada(int posicao){
-        return posicao >= 0 && posicao < this.totalDeAlunos;
+        //return posicao >= 0 && posicao < this.totalDeAlunos;
+        return posicao < this.alunos.length && posicao >= 0;
+
+    }
+
+    private boolean posicaoValida(int posicao) {
+        return posicao >= 0 && posicao <= this.totalDeAlunos;
     }
 
     public void remove(int posicao){
-        //implementar
+        if (!this.posicaoOcupada(posicao)) {
+            throw new IllegalArgumentException("Posição inválida!");
+        }
+        for (int i = posicao; i < this.totalDeAlunos - 1; i++) {
+            this.alunos[i] = this.alunos[i + 1];
+        }
+        this.totalDeAlunos--;
     }
 
-    public  boolean contem(Aluno aluno){
+    public  boolean contem(String aluno){
         for (int i = 0; i < this.totalDeAlunos; i++) {
-            if (aluno.equals(this.alunos[i])) {
+            if (aluno.toLowerCase().equals(this.alunos[i].getNome().toLowerCase())) {
                 return true;
             }
         }
@@ -61,6 +87,16 @@ public class Vetor {
         }
         builder.append("]");
         return builder.toString();
+    }
+
+    private void garantaEspaco() {
+        if (this.totalDeAlunos == this.alunos.length) {
+            Aluno[] novaArray = new Aluno[this.alunos.length * 2];
+            for (int i = 0; i < this.alunos.length; i++) {
+                novaArray[i] = this.alunos[i];
+            }
+            this.alunos = novaArray;
+        }
     }
 
 }
